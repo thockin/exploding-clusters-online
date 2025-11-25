85d0ff9fc5de631a63c367249348c681838f66bd
Tim Hockin <thockin@google.com>
2025-11-25 09:36:13 -0800

    Rounded corners on cards

--- online/render_cards.sh
+++ online/render_cards.sh
@@ -7,12 +7,23 @@ mkdir -p cards
 for f; do
     out=$(basename "$f" | sed 's/\.svg$/.png/')
     echo "$f -> ./cards/$out"
+
+    # Render with no background...
     inkscape \
         --export-area-page \
         --export-dpi=450 \
-        --export-background=white \
+        --export-background-opacity=0 \
         -w 1024 \
         "$f" \
-        -o "./cards/$out"
+        -o "/tmp/tmp_$out"
+    # ...then add a background with rounded corners.
+    convert \
+        card_bg.png \
+        +set date:timestamp \
+        -gravity center \
+        -compose Atop \
+        "/tmp/tmp_$out" \
+        -composite \
+        "./cards/$out"
 done
 
